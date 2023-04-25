6a720df60c71be6c6d1ab2f12bbe40fd1c8330b8
Lufe04 <128490642+Lufe04@users.noreply.github.com>
2023-04-25 10:35:34 -0500

    Delete Documents directory

--- Documents/NetBeansProjects/LasInterfacces/src/Interfaces/PropiedadesFigura.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-public interface PropiedadesFigura {
-    //firmas del metodo
-    //en las interfaces van los metodos que cada hijo puede necesitas diferente, entonces no se ponen en la clase padre
-    public abstract float getPerimetro();
-    public abstract float getArea();
-    public abstract float getVolumen();
-}
--- Documents/NetBeansProjects/POO_1/src/MetodoConstructor/Main.java
+++
@@ -1,8 +0,0 @@
-package MetodoConstructor;
-
-public class Main {
-    public static void main(String [] args){
-        Persona p1 = new Persona("Alejandro",21);
-        p1.mostrarDatos();
-    }
-}
